f31a9f1b4957773427dc1a83ded3029e53b501b8
GerenP <147972228+GerenP@users.noreply.github.com>
2024-07-17 11:37:45 +0700

    initialize

---
+++ editpasien.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="row mt-3">
+        <div class="col-4">
+            <h3>Edit Data Pasien</h3>
+            <?php
+            include 'koneksi.php';
+            $panggil = $koneksi->query("SELECT * FROM pasien where idPasien='$_GET[edit]'");
+
+            while ($row = $panggil->fetch_assoc()) {
+            ?>
+            <form action="koneksi.php" method="POST">
+                <div class=form-group>
+                    <label for="idPasien">ID Pasien</label>
+                    <input type="text" class="form-control mb-3" name="idPasien" placeholder="ID Pasien" value="<?= $row['idPasien'] ?>" readonly>
+                </div>
+                <div class=form-group>
+                    <label for="nmPasien">Nama Pasien</label>
+                    <input type="text" class="form-control mb-3" name="nmPasien" placeholder="Nama Pasien" value="<?= $row['nmPasien'] ?>">
+                </div>
+                <div class=form-group>
+                    <label for="jk">Jenis Kelamin</label>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" name="jk" value="Perempuan" <?php if (($row['jk']) === "Perempuan") {
+                            echo "checked";
+                        }?>>Perempuan
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" name="jk" value="Perempuan" <?php if (($row['jk']) === "Laki-Laki") {
+                            echo "checked";
+                        }?>>Laki-Laki
+                    </div>
+                </div>
+                <div class="form-group mt-3">
+                    <label for="alamat">Alamat</label>
+                    <textarea name="alamat" id="alamat" cols="5" rows="3" placeholder="Alamat" class="form-control"<?= $row['alamat'] ?>></textarea>
+                </div>
+            </form>
+            <?php    }   ?>
+        </div>
+    </div>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,42 @@
+    <?php
+    $koneksi = new mysqli('localhost', 'root', '', 'geren_11pplg1')
+        or die(mysqli_error($koneksi));
+
+
+    if (isset($_POST['simpan'])) {
+        $idPasien = $_POST['idPasien'];
+        $nmPasien = $_POST['nmPasien'];
+        $jk = $_POST['jk'];
+        $alamat = $_POST['alamat'];
+        $koneksi->query("INSERT INTO pasien (idPasien, nmPasien, jk, alamat) values('$idPasien', '$nmPasien', '$jk', '$alamat')");
+
+        header('location:pasien.php');
+    }
+
+
+    if (isset($_GET['idPasien'])) {
+        $idPasien = $_GET['idPasien'];
+        $koneksi->query("DELETE FROM pasien where idPasien = 'idPasien'");
+        header ("location:pasien.php");
+    }
+
+    if (isset($_POST['edit'])) {
+        $idPasien = $_POST['idPasien'];
+        $nmPasien = $_POST['nmPasien'];
+        $jk = $_POST['jk'];
+        $alamat = $_POST['alamat'];
+
+        $koneksi->query("UPDATE pasien SET idPasien='$idPasien', nmpasien='$nmPasien', jk='$jk', alamat='$alamat'");
+        header("location:pasien.php");
+    }
+    if (isset($_GET['idPasien'])) {
+        $idPasien = $_GET['idPasien'];
+        $koneksi->query("DELETE FROM pasien WHERE idPasien = '$idPasien'");
+    
+        // Set session variable for deletion notification
+        $_SESSION['deleted'] = true;
+    
+        header("Location: pasien.php");
+        exit();
+    }
+    ?>
